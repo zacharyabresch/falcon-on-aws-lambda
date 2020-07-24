911bb4d5b2a54d768a858b60347c0b6fd970eba2
Zachary Abresch <zachary.abresch@gmail.com>
2020-07-24 23:38:31 +0000

    Add archive file creation to config
    - If this works it is _so_ freaking cool

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -1,5 +1,14 @@
+data "archive_file" "dummy" {
+  type        = "zip"
+  output_path = "${path.module}/build.zip"
+
+  source {
+    content  = "hello dummy"
+    filename = "dummy.txt"
+  }
+}
 resource "aws_lambda_function" "this" {
-  filename      = pathexpand("../dist/build.zip")
+  filename      = data.archive_file.dummy.output_path
   function_name = "falcon-on-aws-lambda"
   role          = aws_iam_role.this.arn
   handler       = "src.service.handler"
