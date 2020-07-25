509b41a902cbfe9dfec4d47843242dfa0bad86ae
Zachary Abresch <zachary.abresch@gmail.com>
2020-07-25 17:43:02 +0000

    Modify http types

--- terraform/api-gateway.tf
+++ terraform/api-gateway.tf
@@ -12,7 +12,7 @@ resource "aws_api_gateway_resource" "degs" {
 resource "aws_api_gateway_method" "degs" {
   rest_api_id   = aws_api_gateway_rest_api.this.id
   resource_id   = aws_api_gateway_resource.degs.id
-  http_method   = "GET"
+  http_method   = "ANY"
   authorization = "NONE"
 }
 
@@ -21,7 +21,7 @@ resource "aws_api_gateway_integration" "this" {
   resource_id = aws_api_gateway_resource.degs.id
   http_method = aws_api_gateway_method.degs.http_method
 
-  integration_http_method = "GET"
+  integration_http_method = "POST"
   type                    = "AWS_PROXY"
   uri                     = aws_lambda_function.this.invoke_arn
 }
