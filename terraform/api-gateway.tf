bd3db3f53ee81cce9855809333ee2c810e9c2e7f
Zachary Abresch <zachary.abresch@gmail.com>
2020-07-25 17:17:43 +0000

    Fix wrong identifier

--- terraform/api-gateway.tf
+++ terraform/api-gateway.tf
@@ -11,7 +11,7 @@ resource "aws_api_gateway_resource" "degs" {
 
 resource "aws_api_gateway_method" "degs" {
   rest_api_id   = aws_api_gateway_rest_api.this.id
-  resource_id   = aws_api_gateway_resource.this.id
+  resource_id   = aws_api_gateway_resource.degs.id
   http_method   = "GET"
   authorization = "NONE"
 }
