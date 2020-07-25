f6f3a9f7acdcc9d3c8e5afbf674b31005971ed0d
Zachary Abresch <zachary.abresch@gmail.com>
2020-07-25 17:15:24 +0000

    Coeect resource ids

--- terraform/api-gateway.tf
+++ terraform/api-gateway.tf
@@ -4,21 +4,21 @@ resource "aws_api_gateway_rest_api" "this" {
 }
 
 resource "aws_api_gateway_resource" "degs" {
-  rest_api_id = aws_lambda_function.this.id
+  rest_api_id = aws_api_gateway_rest_api.this.id
   parent_id   = aws_api_gateway_rest_api.this.root_resource_id
   path_part   = "degs"
 }
 
 resource "aws_api_gateway_method" "degs" {
-  rest_api_id   = aws_lambda_function.this.id
-  resource_id   = aws_api_gateway_rest_api.this.id
+  rest_api_id   = aws_api_gateway_rest_api.this.id
+  resource_id   = aws_api_gateway_resource.this.id
   http_method   = "GET"
   authorization = "NONE"
 }
 
 resource "aws_api_gateway_integration" "this" {
-  rest_api_id = aws_lambda_function.this.id
-  resource_id = aws_api_gateway_method.degs.resource_id
+  rest_api_id = aws_api_gateway_rest_api.this.id
+  resource_id = aws_api_gateway_resource.degs.id
   http_method = aws_api_gateway_method.degs.http_method
 
   integration_http_method = "GET"
@@ -29,9 +29,14 @@ resource "aws_api_gateway_integration" "this" {
 resource "aws_api_gateway_deployment" "this" {
   depends_on = [
     aws_api_gateway_integration.this
+    aws_api_gateway_method.this
   ]
 
   rest_api_id = aws_api_gateway_rest_api.this.id
   stage_name  = "default"
+}
+
+output "gateway-url" {
+  value = aws_api_gateway_deployment.invoke_url
 }
 
